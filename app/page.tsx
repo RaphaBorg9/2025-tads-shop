a84ff8ff2226baacb6283a575dafebbebe534b1e
RaphaBorg9 <amaisprotegida@gmail.com>
2025-06-10 22:31:48 -0300

    git meu inimigo ultimo commit 10-06

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,10 @@
-<<<<<<< HEAD
-
-
 export default async function HomePage() {
    let response = await fetch("http://localhost:3002/marcas");
     let marcas = await response.json();
   return (
     <div className="p-4">
-      <h1>Página Iniciall</h1>
-     <ul>
-=======
-export default async function HomePage() {
- let response = await fetch("http://localhost:3002/marcas");
- let marcas = await response.json(); 
-  return (
-    <div className="p-4">
       <h1>Página Inicial</h1>
-       <ul>
->>>>>>> abb584fb4adfeca91189ad3c9d722f38f8f7a411
+     <ul>
           {marcas.map((marca) => (
             <li key={marca.id}>{marca.nome}</li>
           ))}
@@ -24,3 +12,4 @@ export default async function HomePage() {
     </div>
   );
 }
+
